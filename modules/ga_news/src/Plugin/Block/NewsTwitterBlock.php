c2f730582e52d22e0afb123412b4011fe3369185
baptiste <baptiste@bmagic.fr>
2016-08-13 00:56:08 +0200

    End of News block on Front page

--- modules/ga_news/src/Plugin/Block/NewsTwitterBlock.php
+++ modules/ga_news/src/Plugin/Block/NewsTwitterBlock.php
@@ -25,6 +25,7 @@ class NewsTwitterBlock extends BlockBase
         $newsNids = \Drupal::entityQuery('node')
             ->condition('status', 1)
             ->condition('type', 'news')
+            ->sort('created','DESC')
             ->range(0, 3)
             ->execute();
 
@@ -33,10 +34,16 @@ class NewsTwitterBlock extends BlockBase
         $langcode = \Drupal::languageManager()->getCurrentLanguage(LanguageInterface::TYPE_CONTENT)->getId();
         foreach ($newsNids as $nid) {
             $node = Node::load($nid)->getTranslation($langcode);
+
+            $tag = \Drupal\taxonomy\Entity\Term::load($node->get("field_news_tags")->getValue()[0]['target_id']);
             $news[] = array(
+                "nid" => $node->id(),
                 "title" => $node->getTitle(),
                 "image" => ImageStyle::load('news_front')->buildUrl($node->get("field_news_image")->entity->uri->value),
                 "text" => $node->get("field_news_content")->getValue()[0]['summary'],
+                "tag" => $tag->getName(),
+                "tagColor" =>$tag->get("field_tags_color")->getValue()[0]['value'],
+                "url" => $node->url()
             );
         }
 
