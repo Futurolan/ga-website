43d0526a5df71288fb83740639ed069840fd1d83
baptiste <baptiste@bmagic.fr>
2016-10-14 23:17:40 +0200

    Create planning page starting kit

---
+++ modules/custom/ga_planning/src/Controller/PlanningController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Drupal\ga_planning\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\ga_planning\PlanningUtils;
+
+class PlanningController extends ControllerBase
+{
+    public function render()
+    {
+        $config = PlanningUtils::getConfiguration();
+        return array(
+            '#theme' => "ga_planning",
+            '#config' => $config
+        );
+    }
+
+}
---
+++ modules/custom/ga_planning/src/PlanningUtils.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Drupal\ga_planning;
+
+class PlanningUtils
+{
+
+    public static function getConfiguration()
+    {
+        $config = \Drupal::config('ga_planning.settings');
+
+        $variables['title'] = $config->get('title');
+
+        return $variables;
+    }
+}
