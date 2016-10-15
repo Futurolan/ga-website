87cc62851db99e71db0858f7a18b8cc362351d92
baptiste <baptiste@bmagic.fr>
2016-10-15 09:45:16 +0200

    Fix bug #76

--- themes/custom/gamers_assembly/components/slideshow/slideshow.js
+++ themes/custom/gamers_assembly/components/slideshow/slideshow.js
@@ -2,16 +2,16 @@
     'use strict';
 
     $('.home-slider-container').loopslider({
-        autoplay: true
-        ,visibleItems: 1
-        ,step: 1
-        ,slideDuration: 500 // Slide transition duration (in ms)
-        ,autoplayInterval: 300000 // Delay between slides
-        ,stopOnHover: true // Stop slideshow on mouse over
-        ,navigation: true // prev and next buttons
-        ,pagination: false
-        ,prevButton: '#home-slideshow-prev' // CSS selector for element used to populate the "Prev" control
-        ,nextButton: '#home-slideshow-next' // CSS selector for element used to populate the "Next" control
+        autoplay: true,
+        visibleItems: 1,
+        step: 1,
+        slideDuration: 500, // Slide transition duration (in ms)
+        autoplayInterval: 300000, // Delay between slides
+        stopOnHover: true, // Stop slideshow on mouse over
+        navigation: true, // prev and next buttons
+        pagination: false,
+        prevButton: '#home-slideshow-prev', // CSS selector for element used to populate the "Prev" control
+        nextButton: '#home-slideshow-next' // CSS selector for element used to populate the "Next" control
     });
 
 })(jQuery);
