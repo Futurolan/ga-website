ca0b0268637444f9f824b8b35475c9a816db9f23
baptiste <baptiste@bmagic.fr>
2016-11-04 23:34:09 +0100

    Add visitor full config

--- modules/custom/ga_visitors/src/VisitorsUtils.php
+++ modules/custom/ga_visitors/src/VisitorsUtils.php
@@ -10,6 +10,15 @@ class VisitorsUtils
         $config = \Drupal::config('ga_visitors.settings');
 
         $variables['title'] = $config->get('title');
+        $variables['description'] = $config->get('description');
+        $variables['map_api_key'] = $config->get('map_api_key');
+        $variables['map_place_id'] = $config->get('map_place_id');
+        $variables['planning_text'] = $config->get('planning_text');
+        $variables['price_text'] = $config->get('price_text');
+        $variables['access_text'] = $config->get('access_text');
+        $variables['accommodation_text'] = $config->get('accommodation_text');
+        $variables['equipment_text'] = $config->get('equipment_text');
+        $variables['food_service_text'] = $config->get('food_service_text');
 
         return $variables;
     }
