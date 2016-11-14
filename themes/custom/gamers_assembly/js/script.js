4f8ac8bd1916d7d8e69c67341cebf38b5af6be58
Alogon <ae.alogon@gmail.com>
2016-11-14 23:54:59 +0100

    Video Slideshow

--- themes/custom/gamers_assembly/js/script.js
+++ themes/custom/gamers_assembly/js/script.js
@@ -3,15 +3,8 @@
 
 
     function stickyFooter() {
-    	console.log($("body").height());
     	var footerH = $(".menu-footer").outerHeight();
     	$("body").css("margin-bottom", footerH+"px");
-        /*var wrapper = document.getElementById('body');
-        var push = document.getElementById('sticky-push');
-        var footer = document.getElementById('sticky-footer');
-
-        wrapper.style.margin = "0 auto -"+footer.offsetHeight+'px';
-        push.style.height = footer.offsetHeight+'px';*/
     }
 
     stickyFooter();
