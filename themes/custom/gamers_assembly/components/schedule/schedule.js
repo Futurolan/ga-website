56527d0d251886dae31d2f05ddac86617b121dc4
baptiste <baptiste@bmagic.fr>
2017-03-07 22:49:55 +0100

    Schedule

--- themes/custom/gamers_assembly/components/schedule/schedule.js
+++ themes/custom/gamers_assembly/components/schedule/schedule.js
@@ -1,19 +1,29 @@
 (function ($) {
     'use strict';
 
-    var container = document.getElementById('timeline');
 
-    console.log(activityDay);
+    var uniqueRooms = []
+    $.each(rooms, function (i, el) {
+        if ($.inArray(el, uniqueRooms) === -1) uniqueRooms.push(el);
+    });
+
+    var groups = new vis.DataSet();
+    uniqueRooms.forEach(function (room) {
+        groups.add({id: room, content: room})
+    });
+
+    //Init the timeline on first day
+    var day = Object.keys(activityDay)[0];
+    createTimeline(day);
+
+
+    //Filter function
     $('.filter').click(function () {
         day = $(this).attr('x-data');
         createTimeline(day);
     });
 
-
-    var day = Object.keys(activityDay)[0];
-
-    createTimeline(day);
-
+    //Timeline creation
     function createTimeline(day) {
         var data = new vis.DataSet(activityDay[day]);
         var options = {
@@ -24,11 +34,13 @@
         };
 
         $('#timeline').empty();
-        var timeline = new vis.Timeline(container, data, options);
-        timeline.setWindow(new Date(day), addDays(day,1), {animation: false});
+        var timeline = new vis.Timeline( document.getElementById('timeline'), data, options);
+        timeline.setWindow(new Date(day), addDays(day, 1), {animation: false});
+        timeline.setGroups(groups);
 
     }
 
+    //Add a day to a date
     function addDays(date, days) {
         var result = new Date(date);
         result.setDate(result.getDate() + days);
