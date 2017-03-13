385df510c37b594171fcaf07a3fc22fe937b4be4
baptiste <baptiste@bmagic.fr>
2017-03-13 23:22:00 +0100

    schedule

--- themes/custom/gamers_assembly/components/schedule/schedule.js
+++ themes/custom/gamers_assembly/components/schedule/schedule.js
@@ -1,64 +1,44 @@
 (function ($) {
     'use strict';
 
-
-    var uniqueRooms = []
-    $.each(rooms, function (i, el) {
-        if ($.inArray(el, uniqueRooms) === -1) uniqueRooms.push(el);
-    });
-
-    var groups = new vis.DataSet();
-    uniqueRooms.forEach(function (room) {
-        groups.add({id: room, content: room})
-    });
-
-    //Init the timeline on first day
-    var day = Object.keys(activityDay)[0];
-    createTimeline(day);
+    var uniqueRooms = _.unique(rooms,'id')
+    var groups = new vis.DataSet(uniqueRooms);
 
 
     //Filter function
     $('.filter').click(function () {
-        day = $(this).attr('x-data');
+        var day = $(this).attr('x-data');
         $('.filter').removeClass('active');
+
+        timeline.setWindow(day, addDays(day,1));
+
         $(this).addClass('active');
-        createTimeline(day);
     });
 
 
-    //Timeline creation
-    function createTimeline(day) {
+    var data = new vis.DataSet(activities);
+    var options = {
+        zoomMin: 1000 * 60 * 60 * 4,
+        zoomMax: 1000 * 60 * 60 * 24 * 3,
+        rollingMode: false,
+        hiddenDates: [{start: '2017-04-15 00:00:00', end: '2017-04-15 08:00:00', repeat: 'daily'}],
+        stack: false,
+        groupOrder: function (a, b) {
+            return a.value - b.value;
+        },
+    };
 
-        var min = addDays(day, 10);
-        var max = addDays(day, -10)
+    var timeline = new vis.Timeline(document.getElementById('timeline'), data, options);
+    timeline.setGroups(groups);
 
-        activityDay[day].forEach(function (activity) {
-            if (activity.start < min)
-                min = activity.start;
-            if (activity.end > max)
-                max = activity.end;
-        });
-        var data = new vis.DataSet(activityDay[day]);
-        var options = {
-            zoomMin: max.getTime()-min.getTime(),
-            zoomMax: max.getTime()-min.getTime(),
-            min: min,
-            max: max
-        };
+    timeline.on('select', function (properties) {
+        activities.forEach(function (activity) {
+            if (activity.id === properties.items[0] && activity.url) {
+                window.location = activity.url
+            }
+        })
+    });
 
-        $('#timeline').empty();
-        var timeline = new vis.Timeline(document.getElementById('timeline'), data, options);
-        timeline.setWindow(new Date(day), addDays(day, 1), {animation: false});
-        timeline.setGroups(groups);
-
-        timeline.on('select', function (properties) {
-            activityDay[day].forEach(function (activity) {
-                if (activity.id === properties.items[0] && activity.url) {
-                    window.location = activity.url
-                }
-            })
-        });
-    }
 
     //Add a day to a date
     function addDays(date, days) {
