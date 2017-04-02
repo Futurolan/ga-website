2ed5e22271e0aec6bb792a6cce5ecf8f302c134e
baptiste <baptiste@bmagic.fr>
2017-04-02 22:00:29 +0200

    Fix https on twitch front player

--- themes/custom/gamers_assembly/libraries/chat/chat.js
+++ themes/custom/gamers_assembly/libraries/chat/chat.js
@@ -23,6 +23,7 @@
         showConnectionNotices = true; // Show messages like "Connected" and "Disconnected"
 
 
+
     var chat = document.getElementById('chat'),
         defaultColors = ['rgb(255, 0, 0)', 'rgb(0, 0, 255)', 'rgb(0, 128, 0)', 'rgb(178, 34, 34)', 'rgb(255, 127, 80)', 'rgb(154, 205, 50)', 'rgb(255, 69, 0)', 'rgb(46, 139, 87)', 'rgb(218, 165, 32)', 'rgb(210, 105, 30)', 'rgb(95, 158, 160)', 'rgb(30, 144, 255)', 'rgb(255, 105, 180)', 'rgb(138, 43, 226)', 'rgb(0, 255, 127)'],
         randomColorsChosen = {},
@@ -30,7 +31,11 @@
             options: {
                 debug: true
             },
-            channels: channels
+            channels: channels,
+            connection: {
+                reconnect:true,
+                secure: true
+            }
         },
         client = new tmi.client(clientOptions);
 
