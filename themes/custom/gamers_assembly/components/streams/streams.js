07a057810d40fc2bb80987bc93d8f7aeeb2d5fa4
baptiste <baptiste@bmagic.fr>
2017-04-10 23:00:20 +0200

    Fix FF bug stream

--- themes/custom/gamers_assembly/components/streams/streams.js
+++ themes/custom/gamers_assembly/components/streams/streams.js
@@ -1,12 +1,14 @@
 (function ($) {
     'use strict';
     var twitch_key;
+    var chat_key;
 
     $(".stream-video-list .stream-video-list-item").each(function () {
 
         var key = $(this).attr("x-key");
-        if (twitch_key == null)
+        if (twitch_key == null) {
             twitch_key = key;
+        }
         $.ajax({
             url: "https://api.twitch.tv/kraken/streams/" + key + "?client_id=3e1bo4w4sfblm61c8baoacmcxab9dq",
         }).done(function (data) {
@@ -37,7 +39,10 @@
     $(".stream-tab-chat").click(function () {
         $(".stream-video-list, .stream-video-more").hide();
         $(".stream-chat").show();
-        $(".stream-chat iframe").prop('src', "https://www.twitch.tv/" + twitch_key + "/chat?darkpopout");
+        if (chat_key != twitch_key) {
+            $(".stream-chat iframe").prop('src', "https://www.twitch.tv/" + twitch_key + "/chat?darkpopout");
+            chat_key = twitch_key;
+        }
         $(".stream-tab-video-list").removeClass("active");
         $(".stream-tab-chat").addClass("active");
     });
