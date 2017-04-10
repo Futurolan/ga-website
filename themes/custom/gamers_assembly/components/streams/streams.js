704a32dae8a92c375d67b230fb8b24f3932fce06
preprod-ga <preprod-ga>
2017-04-11 01:25:49 +0200

    Damn FF bug

--- themes/custom/gamers_assembly/components/streams/streams.js
+++ themes/custom/gamers_assembly/components/streams/streams.js
@@ -1,16 +1,10 @@
 (function ($) {
     'use strict';
-    var twitch_key;
-    var chat_key;
 
     $(".stream-video-list .stream-video-list-item").each(function () {
-
         var key = $(this).attr("x-key");
-        if (twitch_key == null) {
-            twitch_key = key;
-        }
         $.ajax({
-            url: "https://api.twitch.tv/kraken/streams/" + key + "?client_id=3e1bo4w4sfblm61c8baoacmcxab9dq",
+            url: "https://api.twitch.tv/kraken/streams/" + key+"?client_id=3e1bo4w4sfblm61c8baoacmcxab9dq",
         }).done(function (data) {
             if (data && data.stream) {
                 $("#stream-video-list-item-" + key).addClass('online');
@@ -24,38 +18,36 @@
     });
 
     $(".stream-video-list .stream-video-list-item").click(function () {
-        twitch_key = $(this).attr("x-key");
-        $(".stream-video iframe").prop('src', "https://player.twitch.tv/?channel=" + twitch_key);
+        var key = $(this).attr("x-key");
+        $(".stream-video iframe").prop('src', "https://player.twitch.tv/?channel=" + key);
+        $(".stream-chat iframe").prop('src', "https://www.twitch.tv/"+key+"/chat?darkpopout");
         $(".stream-video-list-item").removeClass('active');
-        $("#stream-video-list-item-" + twitch_key).addClass('active');
+        $("#stream-video-list-item-" + key).addClass('active');
     });
 
     $(".stream-tab-video-list").click(function () {
-        $(".stream-video-list, .stream-video-more").show();
-        $(".stream-chat").hide();
+        $(".stream-chat").css({position:"absolute"});
+        $(".stream-chat").css({top:-5000+"px"});
+        $(".stream-video-list").css({position:"static"});
         $(".stream-tab-video-list").addClass("active");
         $(".stream-tab-chat").removeClass("active");
     });
     $(".stream-tab-chat").click(function () {
-        $(".stream-video-list, .stream-video-more").hide();
-        $(".stream-chat").show();
-        if (chat_key != twitch_key) {
-            $(".stream-chat iframe").prop('src', "https://www.twitch.tv/" + twitch_key + "/chat?darkpopout");
-            chat_key = twitch_key;
-        }
+        $(".stream-video-list").css({position:"absolute"});
+        $(".stream-video-list").css({top:-5000+"px"});
+        $(".stream-chat").css({position:"static"});
         $(".stream-tab-video-list").removeClass("active");
         $(".stream-tab-chat").addClass("active");
     });
 
-    $('.stream-video-more').click(function () {
+    $('.stream-video-more').click(function() {
         if ($(this).hasClass('active')) {
             $(this).removeClass('active');
-            $('.stream-video-list').height('200px');
+            $('.stream-video-list-content').height('200px');
         } else {
             $(this).addClass('active');
-            $('.stream-video-list').height('auto');
+            $('.stream-video-list-content').height('auto');
         }
     });
-
 
 })(jQuery);
