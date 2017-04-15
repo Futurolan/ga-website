2aeb721251f7d6b92d2be49bccc40cdb19b7b26a
baptiste <baptiste@bmagic.fr>
2017-04-15 18:12:24 +0200

    Fix next activities bug (date filter D8 not operational)

--- themes/custom/gamers_assembly/components/tournaments-home-next/tournaments-home-next.js
+++ themes/custom/gamers_assembly/components/tournaments-home-next/tournaments-home-next.js
@@ -9,18 +9,21 @@
         }).done(function (data) {
             var str = '';
             $.each(data, function (index, activity) {
-                str += '<tr>';
-                str += '<td class="date">' + activity.field_date + '</td>';
-                if(activity.field_lien && activity.field_lien != ''){
-                    str += '<td class="title"><a href="'+activity.field_lien+'">'+ activity.title + '</a></td>';
-                }else {
-                    str += '<td class="title">' + activity.title + '</td>';
+
+                if(new Date(activity.field_date_1)>new Date()) {
+                    str += '<tr>';
+                    str += '<td class="date">' + activity.field_date + '</td>';
+                    if (activity.field_lien && activity.field_lien != '') {
+                        str += '<td class="title"><a href="' + activity.field_lien + '">' + activity.title + '</a></td>';
+                    } else {
+                        str += '<td class="title">' + activity.title + '</td>';
+                    }
+                    str += '<td class="room">' + activity.field_room + '</td>';
+                    str += '</tr>';
                 }
-                str += '<td class="room">' + activity.field_room + '</td>';
-                str += '</tr>';
             });
             $('.tournaments-home-next table').html(str);
-            setTimeout(setNextActivities,60000);
+            setTimeout(setNextActivities, 10 * 60 * 1000); //each 10 min
         });
 
     }
